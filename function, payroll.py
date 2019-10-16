c5755a55581720ddcbaac28ede54c8bd7aba48c6
Jayanthsrinivas11 <56616046+Jayanthsrinivas11@users.noreply.github.com>
2019-10-15 23:25:58 -0230

    Create function, payroll.py
    so, this is a small project I am trying to do to understand how functions works in Python.
    This is a code to calculate the payroll, with the rate per hour being 10.50, and the regular hours worked being 40. however, if the worker does overtime, he must be payed 1.5$ more per each extra hour he works.
    now, the problem I am having is, that I am unable to get any result. I am having problems with my return, and print statements.
    any help on how I should do this properly, would be very appreciated..

---
+++ function, payroll.py
@@ -0,0 +1,13 @@
+def pay(h,r):
+    if hours>40:
+        print(float(hours-40)*float(1.5)*float(rate)+40*float(rate))
+    else:
+        print(float(hours)*float(rate)
+    return = pay
+
+
+hours = input('please enter the number of hours you worked?')
+rate = input('please enter the rate of pay per hour?')
+print(pay)
+
+   
